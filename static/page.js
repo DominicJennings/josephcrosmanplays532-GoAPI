5b3d430d10777ce3e06efbb3fae1cac4efbec92e
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-05-01 17:47:16 -0400

    Update page.js

--- static/page.js
+++ static/page.js
@@ -179,7 +179,7 @@ module.exports = function (req, res, url) {
 	res.end(
 		`<script>document.title='${title}',flashvars=${JSON.stringify(
 			params.flashvars
-		)}</script><div id="studio_holder" style="width: 1358px;">${toObjectString(attrs, params)}</div>${stuff.pages[url.pathname] || ""}`
+		)}</script><body style="margin:0px">${toObjectString(attrs, params)}</body>${stuff.pages[url.pathname] || ""}`
 	);
 	return true;
 };
